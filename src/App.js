559b547882675e56c1a6eb9b00635954d9752c59
anmolkush <120050748+anmolkush@users.noreply.github.com>
2023-09-23 14:52:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from "./Components/todoreact/Todo"
+
+function App() {
+  return (
+    <>
+      <Todo></Todo>
+    </>
+  );
+}
+
+export default App;
